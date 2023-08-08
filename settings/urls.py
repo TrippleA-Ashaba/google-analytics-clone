de48910ab0307c0bdaf4803ad443c1107ef03afe
TrippleA-Ashaba <trippleaunit@hotmail.com>
2023-08-08 11:45:37 +0300

    Add browser-reload and debug-toolbar

--- settings/urls.py
+++ settings/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+urlpatterns = [path("admin/", admin.site.urls), path("", include("apps.core.urls"))]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+        path("__reload__/", include("django_browser_reload.urls")),
+    ]
