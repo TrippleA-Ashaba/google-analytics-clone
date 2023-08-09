7d2dc8aae008e9a6f6a49390b065983d7c5b4d1f
TrippleA-Ashaba <trippleaunit@hotmail.com>
2023-08-09 11:38:07 +0300

    Add sneak template

--- settings/settings.py
+++ settings/settings.py
@@ -118,10 +118,19 @@ USE_I18N = True
 USE_TZ = True
 
 
+# This specifies the absolute filesystem path to the directory
+# where user-uploaded media files will be stored.
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+# This specifies the base URL for media files.
+MEDIA_URL = "/media/"
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [BASE_DIR / "static/"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
