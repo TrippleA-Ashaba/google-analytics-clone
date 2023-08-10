a15a66b60ea77275767946682f926622b130585d
TrippleA-Ashaba <trippleaunit@hotmail.com>
2023-08-10 12:43:57 +0300

    Create User , Business and Staff

--- settings/settings.py
+++ settings/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # local
+    "apps.accounts",
     "apps.core",
 ]
 
@@ -136,3 +137,5 @@ STATICFILES_DIRS = [BASE_DIR / "static/"]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.CustomUser"
