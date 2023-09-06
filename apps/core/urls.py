c43226cc33caa23c9161e4910bd1b76a00cbc09e
TrippleA-Ashaba <trippleaunit@hotmail.com>
2023-09-06 13:16:03 +0300

    Add instructions and sample dashboard

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -19,10 +19,14 @@ from .views import (
     show_business,
     show_properties,
     staff_add,
+    instructions,
+    sample_dashboard,
 )
 
 urlpatterns = [
     path("", landing_page, name="landing_page"),
+    path("instructions/", instructions, name="instructions"),
+    path("sample_dashboard/", sample_dashboard, name="sample_dashboard"),
     # ============== Dashboard =======================
     path("dashboard/", dashboard, name="dashboard"),
     path("property_select/", property_select, name="property_select"),
